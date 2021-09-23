c2934823dac7775a019ed2662f98d16b2f5bda72
tananett <net_tanapon@hotmail.com>
2021-09-23 21:35:59 +0700

    test google maps api on flutter

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyC8fyTP_Bn4lFw9s7vS5fTAQew9FX2lYys")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
